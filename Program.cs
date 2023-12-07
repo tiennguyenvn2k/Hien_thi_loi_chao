0ad1d2123a4312092657fcf373eb6097c9bab66d
tiennguyenvn2k <hiiamgamer2000@gmail.com>
2023-12-07 18:52:22 +0700

    hoan thanh bai tap

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace dotnet_hien_thi_loi_chao;
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name: ");
+        string yourName = Console.ReadLine();
+        Console.WriteLine("Hello " + yourName);
+    }
+}
